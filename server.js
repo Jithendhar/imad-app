4a443a9d80f31c833bafc158b560ac1ad377fa0f
Jithendhar <Jithendharjith@gmail.com>
2018-02-13 15:33:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,17 +6,8 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index1.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index2.html'));
-});
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index3.html'));
-});
-
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
